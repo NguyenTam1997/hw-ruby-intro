2dbf74519d74b7671fab768a70e7de5343a7d8c3
Tam <14020407@vnu.edu.vn>
2016-05-15 12:33:50 +0000

    pass part2_spec

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -20,15 +20,16 @@ end
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s =~ /^[a-z&&[^aeiou]]/i
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return false unless s =~ /^[01]+$/
+    s.to_i(2) % 4 == 0
 end
 
 # Part 3
